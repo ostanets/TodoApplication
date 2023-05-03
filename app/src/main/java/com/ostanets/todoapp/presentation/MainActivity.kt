252eea70e3faf95030e201de735a73bb0c3cf89b
Марат Самегулин <rakarallik@gmail.com>
2023-05-03 21:51:42 +0300

    create main page

--- app/src/main/java/com/ostanets/todoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ostanets/todoapp/presentation/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.ostanets.todoapp.presentation
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.ostanets.todoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_main)
     }
 }
