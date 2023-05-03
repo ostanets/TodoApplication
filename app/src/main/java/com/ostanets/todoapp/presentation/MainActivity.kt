806497a6b23208d23c1e6885c97bad4df1b4149a
Марат Самегулин <rakarallik@gmail.com>
2023-05-03 23:46:07 +0300

    create note list fragment

--- app/src/main/java/com/ostanets/todoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ostanets/todoapp/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.FrameLayout
 import com.ostanets.todoapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame, NoteListFragment.newInstance())
+            .commitAllowingStateLoss()
     }
 }
