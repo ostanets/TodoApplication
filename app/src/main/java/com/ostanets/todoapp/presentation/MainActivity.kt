ff696dfa6b8d61bb8001f14f409f208398434d13
Марат Самегулин <rakarallik@gmail.com>
2023-05-03 18:39:05 +0300

    Initial commit

---
+++ app/src/main/java/com/ostanets/todoapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ostanets.todoapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ostanets.todoapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
