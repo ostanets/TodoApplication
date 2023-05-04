f121f83a0936114ee30751ecd92ec2886e30a6bc
Марат Самегулин <rakarallik@gmail.com>
2023-05-04 11:00:32 +0300

    create note activity

--- app/src/main/java/com/ostanets/todoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ostanets/todoapp/presentation/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.ostanets.todoapp.presentation
 
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.ostanets.todoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_frame, NoteListFragment.newInstance())
-            .commitAllowingStateLoss()
+        val intent = NoteActivity.newIntentAddNote(this)
+        startActivity(intent)
+//        setContentView(R.layout.activity_main)
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.main_frame, NoteListFragment.newInstance())
+//            .commitAllowingStateLoss()
     }
 }
---
+++ app/src/main/java/com/ostanets/todoapp/presentation/NoteActivity.kt
@@ -0,0 +1,52 @@
+package com.ostanets.todoapp.presentation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.ostanets.todoapp.databinding.ActivityNoteBinding
+
+class NoteActivity : AppCompatActivity() {
+    lateinit var viewModel: NoteViewModel
+    lateinit var binding: ActivityNoteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.clearFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        viewModel = ViewModelProvider(this)[NoteViewModel::class.java]
+        binding = ActivityNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.shouldCloseScreen.observe(this) {
+            finish()
+        }
+
+        binding.btnBack.setOnClickListener {
+            viewModel.finishWork()
+        }
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_NOTE_ID = "extra_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
+
+        fun newIntentAddNote(context: Context): Intent {
+            val intent = Intent(context, NoteActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditNote(context: Context, noteId: Int): Intent {
+            val intent = Intent(context, NoteActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_NOTE_ID, noteId)
+            return intent
+        }
+    }
+}
